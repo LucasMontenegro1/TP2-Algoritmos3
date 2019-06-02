31ed7f6a894ad932ff4563e4fee4a3b4a323d287
Marcos <Marcos@192.168.0.16>
2019-06-02 15:37:49 -0300

    Comentarios en herramienta

--- TP2/src/Herramienta.java
+++ TP2/src/Herramienta.java
@@ -1,7 +1,7 @@
 
 public class Herramienta {
-	protected int durabilidad;
-	protected int fuerza;
+	protected int durabilidad; //vida de la herramienta
+	protected int fuerza; //si le pega a algo, cuanto le desgasta
 	
 	public int getDurabilidad() {
 		return this.durabilidad;
