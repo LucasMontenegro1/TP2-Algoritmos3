2dd39eb6a3813cce423375a69c4e76cf2cd3864e
Marcos <Marcos@192.168.0.16>
2019-06-03 12:58:44 -0300

    Se agregaron las pruebas de hacha y pico, ademas de crearse la clase piedra.

--- TP2/src/Hacha.java
+++ TP2/src/Hacha.java
@@ -2,16 +2,20 @@
 public class Hacha extends Herramienta {
 
 	Hacha(Madera madera){
-		this.durabilidad=100;
-		this.fuerza=2;
+		this.durabilidad = 100;
+		this.fuerza = 2;
 		
+	}
+	
+	 Hacha(Piedra piedra) {
+		 this.durabilidad = 200;
+		 this.fuerza = 5;
 	}
 
 	 Hacha(Metal metal) {
-		 this.durabilidad=400;
-		 this.fuerza=10;
-		 //Holissss
+		 this.durabilidad = 400;
+		 this.fuerza = 10;
 	}
-	
-	
+	 
+
 }
--- TP2/src/Pico.java
+++ TP2/src/Pico.java
@@ -1,12 +1,19 @@
 
 public class Pico extends Herramienta {
 	 Pico(Madera madera){
-		 this.durabilidad=100;
-		 this.fuerza=2;
+		 this.durabilidad = 100;
+		 this.fuerza = 2;
 	 }
+	 
+	 Pico(Piedra piedra) {
+		 this.durabilidad = 200;
+		 this.fuerza = 4;
+	}
 
 	 Pico(Metal metal) {
-		 this.durabilidad=400;
-		 this.fuerza=12;
+		 this.durabilidad = 400;
+		 this.fuerza = 12;
 	}
+	 
+
 }
