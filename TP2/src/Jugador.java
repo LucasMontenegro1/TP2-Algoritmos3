df11975546a468f0db4cdfee2b4697bf3445bb0d
Marcos <Marcos@192.168.0.16>
2019-06-09 19:35:50 -0300

    asasas

--- TP2/src/Jugador.java
+++ TP2/src/Jugador.java
@@ -6,11 +6,6 @@ public class Jugador {
 	private Posicion posicion = new Posicion();
 	private Inventario inventario = new Inventario();
 	
-	Jugador(){
-		Madera madera = new Madera();
-		Hacha hacha = new Hacha(madera);
-	}
-	
 	
 	//Hay que ver como van a usarse las herramientas
 	//en el juego para pensar si hace falta o no pasar
