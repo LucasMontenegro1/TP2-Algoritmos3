acb6558393b91b79ef6f1dfc6b080b678d609950
Agustín <Agustn@DESKTOP-43FU4A9>
2019-06-09 18:57:32 -0300

    Cambio cosas

--- TP2/src/Inventario.java
+++ TP2/src/Inventario.java
@@ -1,20 +1,28 @@
 
 public class Inventario {
 
-	private static int cantidadDePaquetes = 28;
-	private Paquete elementosGuardados[] = new Paquete[cantidadDePaquetes];
-	private Equipable elementoSeleccionado;
+    private static int cantidadDePaquetes = 28;
+    private Paquete elementosGuardados[] = new Paquete[cantidadDePaquetes];
+    private Equipable elementoSeleccionado;
 
-	
-	Inventario(){
-		Madera madera = new Madera();
-		elementosGuardados[0].setElementoGuardado(new Hacha(madera));
-		elementoSeleccionado=elementosGuardados[0].getElementoGuardado();
-	}
-	
-	public void usarElementoSeleccionado() {
-		elementoSeleccionado.usar();
-	}
-	 
-	 
+
+    Inventario(){
+        Madera madera = new Madera();
+        getElementosGuardados()[0].setElementoGuardado(new Hacha(madera));
+        elementoSeleccionado = getElementosGuardados()[0].getElementoGuardado();
+    }
+
+    public void usarElementoSeleccionado() {
+        elementoSeleccionado.usar();
+    }
+
+    public Paquete[] getElementosGuardados() {
+        return elementosGuardados;
+    }
+
+    public void setElementosGuardados(Paquete elementosGuardados[]) {
+        this.elementosGuardados = elementosGuardados;
+    }
+
+
 }
