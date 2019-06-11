cbcaf5bf5cd17da2a0e9895eb483566494633e73
Agustín <Agustn@DESKTOP-43FU4A9>
2019-06-10 23:11:27 -0300

    Se agrega TreeMap a receta

--- TP2/src/Modelo/Receta.java
+++ TP2/src/Modelo/Receta.java
@@ -1,5 +1,29 @@
 package Modelo;
 
+
+//import java.util.SortedMap;
+import java.util.TreeMap;
+
+import Herramientas.Herramienta;
+import Materiales.Material;
+
 public class Receta {
 
+	
+	private Herramienta herramientaRepresentada;
+	private TreeMap<Integer, Material> codigoDeCrafteo = new TreeMap<>();
+
+	public bool codigoEsValido(TreeMap<Integer, Material> codigoDeCrafteo) {
+		
+		
+		if(this.codigoDeCrafteo.size() != codigoDeCrafteo.size()) {
+			return false;
+		}
+		
+		
+		
+		
+		
+	}
+	
 }
