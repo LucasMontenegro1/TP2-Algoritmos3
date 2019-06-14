c1eef215e9e99b24ee1dd9484148939fb496e6a8
Marcos <Marcos@192.168.0.16>
2019-06-14 13:24:22 -0300

    Se agrega la funcionalidad de imprimir el mapa nuestro.

--- TP2/src/imagen/ImagenMapa.java
+++ TP2/src/imagen/ImagenMapa.java
@@ -5,10 +5,15 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;    
+import javafx.stage.Stage;
+import mapa.Mapa;
+import Jugador.Jugador;
+import Materiales.*;
 
 
-    public class ImagenMapa extends Application {    
+    public class ImagenMapa extends Application {  
+    	
+    	Mapa mapa = new Mapa();
 
         @Override
         public void start(Stage primaryStage) {
@@ -23,10 +28,19 @@ import javafx.stage.Stage;
                 	
                     Image img = new Image("pasto.jpg");
                 	
-                	if(y == 3 && x == 5) {
+                	if(mapa.getOcupante(y, x).getClass() == Madera.class) {
+                        img = new Image("madera.jpg");
+                	} else if(mapa.getOcupante(y, x).getClass() == Piedra.class) {
                         img = new Image("piedra.jpg");
+                	} else if(mapa.getOcupante(y, x).getClass() == Metal.class) {
+                        img = new Image("metal.jpg");
+                	} else if(mapa.getOcupante(y, x).getClass() == Diamante.class) {
+                        img = new Image("diamante.jpg");
+                	} else if(mapa.getOcupante(y, x).getClass() == Jugador.class) {
+                        img = new Image("steve.jpg");
                 	}
-                	
+                        
+                        
                     ImageView imagen = new ImageView(img);
                     
                     imagen.setPreserveRatio(true);
